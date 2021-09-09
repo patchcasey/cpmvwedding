85fd1b560ca65efd038c3fe46a5dbe28719c17a6
patchcasey <patchcasey@gmail.com>
2021-09-08 21:44:30 -0500

    working homepage, working on wedding page

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -4,6 +4,16 @@ class Home extends Component {
     render() {
         return(
             <div className="condiv">
+                <div className="nameBox">
+                    <span className="marinaText">Marina</span>
+                    <br />
+                    <span className="amp">&</span>
+                    <br />
+                    <span className="caseyText">Casey</span>
+                </div>
+                <div className="nameBox">
+                    <span className="titleDate">September 24, 2022</span>
+                </div>
             </div>
         )
     }
