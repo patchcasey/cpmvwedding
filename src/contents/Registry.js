0a8b1d3608af60210af804b16dff625debd44ff9
play.js <git@gituser.com>
2022-04-02 15:26:23 -0500

    resize registry widget for mobile

--- src/contents/Registry.js
+++ src/contents/Registry.js
@@ -12,18 +12,15 @@ import React, { Component } from 'react';
 // });
 
 class Registry extends Component {
-
-    rawHTML = `<a class="zola-registry-embed" href="www.zola.com/registry/marinaandcasey2022" data-registry-key="marinaandcasey2022">Our Zola Wedding Registry</a><script>!function(e,t,n){var s,a=e.getElementsByTagName(t)[0];e.getElementById(n)||(s=e.createElement(t),s.id=n,s.async=!0,s.src="https://widget.zola.com/js/widget.js",a.parentNode.insertBefore(s,a))}(document,"script","zola-wjs");</script>`
     render() {
         return(
             <div className="condiv">
-                <div className="registryBox">
+               {/* <div className="registryBox"> */}
                     <iframe src={'../zola.html'} 
                     title={"registry"}
-                    // onLoad={'javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));'}
-                    style={{ height: '500px', width:"100%", border:"none", overflow:"hidden"}}
+                    style={{ height: '500px', width:"280px", border:"none", overflow:"hidden"}}
                     ></iframe>
-                </div>
+                {/*</div>*/}
             </div>
         )
     }
