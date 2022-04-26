159429926bde73a3942e0f85dcfbb66a64c8852d
patchcasey <patchcasey@gmail.com>
2022-04-25 20:09:29 -0500

    testing srcDoc for registry iframe

--- src/contents/Registry.js
+++ src/contents/Registry.js
@@ -18,7 +18,8 @@ class Registry extends Component {
                {/* <div className="registryBox"> */}
                     <iframe 
                     className={"registryIFrame"}
-                    src={'../zola.html'}
+                    srcDoc={'<!DOCTYPE html><html><head></head><body><a class="zola-registry-embed" href="www.zola.com/registry/marinaandcasey2022" data-registry-key="marinaandcasey2022">Our Zola Wedding Registry</a><script>!function(e,t,n){var s,a=e.getElementsByTagName(t)[0];e.getElementById(n)||(s=e.createElement(t),s.id=n,s.async=!0,s.src="https://widget.zola.com/js/widget.js",a.parentNode.insertBefore(s,a))}(document,"script","zola-wjs");</script></body></html>'}
+                    // src={'zola.html'}
                     title={"registry"}
                     // style={{ height: '500px', width:"280px", border:"none", overflow:"hidden"}}
                     ></iframe>
